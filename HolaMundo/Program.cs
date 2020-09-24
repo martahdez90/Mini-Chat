1d1c85e1786c6fc8a7422b678001dfc7eae067d4
Marta Hernandez <martahdez90@users.noreply.github.com>
2020-09-24 16:57:52 +0200

    Agregar archivos de proyecto.

---
+++ HolaMundo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace HolaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "John";
+            int age;
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            //Console.WriteLine("Hola soy Marta");
+            Console.WriteLine("Lun 22 jun, 20:00");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Hola Dennis");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Hola " + name);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("...");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("me encantas " + name);
+            age = 16;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("no seas tonto, solo tengo " + age + " años");
+            Console.ReadLine();
+        }
+    }
+}
